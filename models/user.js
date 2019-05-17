98f06e9899781822bd6020aa5b1f9ce598526fa3
KMolina <kevenmolina@gmail.com>
2019-05-17 17:15:14 -0500

    added users and car schema

--- models/user.js
+++ models/user.js
@@ -2,5 +2,29 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    
-})
+    firstName:{
+        type: String,
+        required: true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    age: {
+        type:Number,
+        required: true,
+        min: 21,
+        max: 99
+    },
+    location:{
+        type:String,
+        enum:['Dal','Aus'],
+        required:true
+    },
+    transmission: {
+        type:String,
+        enum:['AUTO','MANUAL','BOTH']
+    }
+    //TODO: add reference to car model for listed vehicles
+},{timestamp:true})
+    module.exports = mongoose.model('User',userSchema)
